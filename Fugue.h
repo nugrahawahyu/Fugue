1f9a3155c3cc9c601b928157d296c9ef3d2855ec
nugrahawahyu <nugraha.c.wahyu@gmail.com>
2016-05-10 16:58:55 +0700

    last good condition

--- Fugue.h
+++ Fugue.h
@@ -2,10 +2,10 @@
 #define _Fugue_h_
 
 /** Libraries */
-#include <Arduino.h>
-#include "StandardCplusplus.h"
+#include "Arduino.h"
+#include <StandardCplusplus.h>
 #include "vector"
-// #include "MemoryFree.h"
+#include "MemoryFree.h"
 
 /** Vendor Libraries */
 #include "vendor/vendor.h"
