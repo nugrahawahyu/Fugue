cc07d3f1a9fd3c4866e37b51f8a22f383aa2f028
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-06-24 10:21:18 +0700

    move get id to line 13

--- vendor/nugraha/fugue/src/Nugraha/Contracts/Devices/DeviceContract.hpp
+++ vendor/nugraha/fugue/src/Nugraha/Contracts/Devices/DeviceContract.hpp
@@ -5,12 +5,12 @@ class DeviceContract
 {
 public:
     virtual ~DeviceContract() {}
-    virtual int getId()=0;
     virtual void turnOn()=0;
     virtual void turnOff()=0;
     virtual void toggle()=0;
     virtual void behavior()=0;
     virtual int getPin()=0;
+    virtual int getId()=0;
     virtual void setLogger(LoggerContract* logger)=0;
     virtual LoggerContract* getLogger()=0;
 };
