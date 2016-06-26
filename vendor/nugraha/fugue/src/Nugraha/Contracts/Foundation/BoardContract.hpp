207f4e8baff5a4f1e9c6577b972dce6741760840
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-06-26 07:42:44 +0700

    change automate, devices, sensors, and services to protected

--- vendor/nugraha/fugue/src/Nugraha/Contracts/Foundation/BoardContract.hpp
+++ vendor/nugraha/fugue/src/Nugraha/Contracts/Foundation/BoardContract.hpp
@@ -2,13 +2,15 @@ namespace Nugraha { namespace Contracts { namespace Foundation {
 
 class BoardContract 
 {
-public:
-    virtual ~BoardContract() {}
+protected:
+    virtual void automate()=0;
     virtual void devices()=0;
     virtual void sensors()=0;
     virtual void services()=0;
+    
+public:
+    virtual ~BoardContract() {}
     virtual void initialize()=0;
-    virtual void automate()=0;
     virtual void executeUserCommand(String userCommands)=0;
 };
 
