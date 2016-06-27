6a2bb0c97ac036ba7580194584ba383b07235b7f
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-06-27 08:53:12 +0700

    use one wire from libraries

--- vendor/milesburton/DallasTemperature/DallasTemperature.h
+++ vendor/milesburton/DallasTemperature/DallasTemperature.h
@@ -19,7 +19,8 @@
 #endif
 
 #include <inttypes.h>
-#include "../../paulStoffregen/OneWire/OneWire.h"
+#include "OneWire.h"
+// #include "../../paulStoffregen/OneWire/OneWire.h"
 
 // Model IDs
 #define DS18S20MODEL 0x10  // also DS1820
