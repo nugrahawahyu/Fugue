eb8b7058b5d65ac7912e757695bb4d445d7fc205
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-07-05 07:39:31 +0700

    remove onewire

--- vendor/vendor.h
+++ vendor/vendor.h
@@ -1,8 +1,6 @@
 #ifndef _vendor_vendor_h_
 #define _vendor_vendor_h_
 
-// #include "paulStoffregen/OneWire/OneWire.h"
-// #include "paulStoffregen/OneWire/OneWire.cpp"
 #include "milesburton/DallasTemperature/DallasTemperature.h"
 #include "milesburton/DallasTemperature/DallasTemperature.cpp"
 #include "bblanchon/ArduinoJson/ArduinoJson.h"
